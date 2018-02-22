1310cfc3ef9bdd29405a30b942a6170a7e7e876c
Unknown <alejaperezh5@gmail.com>
2018-02-22 12:57:19 -0500

    Segundo punto

---
+++ PUNTO UNO.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+
+	string emp;
+	double horasextras;
+	double horastrabajadas;
+	double salario;
+	double tarifaextra;
+	double tarifa;
+	// Salario y tarifa de un trabajador.
+	cout << "Ingrese nombre del empleado" << endl;
+	cin >> emp;
+	cout << "Ingrese numero de horas trabajadas" << endl;
+	cin>>horastrabajadas;
+	cout<< "Ingrese tarifa por hora" << endl;
+    cin >> tarifa;
+	if (horastrabajadas>40) {
+		salario = horastrabajadas*tarifa;
+	} else {
+		tarifaextra = tarifa*1.50;
+		horasextras = horastrabajadas-40;
+		salario = tarifaextra*horasextras+horastrabajadas*tarifa;
+	}
+	cout << "Empleado: " << emp << " Su salario es:    " << salario << " Su tarifa  es:   " << tarifa << " Su tarifa extra es: " << tarifaextra<<  endl;
+	return 0;
+}
+
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float cantidad;
+	int horas;
+	int minutos;
+	float res;
+	int segundos;
+
+	cout << "ingrese tiempo en segundos" << endl;
+	cin >> cantidad;
+	horas = (cantidad/3600);
+	cantidad = cantidad/3600;
+	minutos = ((cantidad- horas * 3600) / 60) ;
+    segundos= cantidad - (horas * 3600 + minutos * 60);
+	cout << horas << " Horas " << minutos << " Minutos " << segundos << " Segundos " << endl;
+	return 0;
+}
+
