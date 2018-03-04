5854a9707061465a58eea63c99d85461b03b1d32
Alejandra pérez <36452227+alejaph@users.noreply.github.com>
2018-03-03 20:23:31 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main() {
 	cin >> cantidad;
 	horas = (cantidad/3600);
 	cantidad = cantidad/3600;
-	minutos = ((cantidad- horas * 3600) / 60) ;
-    segundos= cantidad - (horas * 3600 + minutos * 60);
+	minutos = ((cantidad * horas * 3600) / 60) ;
+    segundos= cantidad * (horas * 3600 + minutos * 60);
 	cout << horas << " Horas " << minutos << " Minutos " << segundos << " Segundos " << endl;
 	return 0;
 }
